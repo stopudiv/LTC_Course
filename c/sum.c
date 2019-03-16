cff723d44184c605e16f687e722d2925a3fd03e8
Olga Grytsenko <ogrytsenko@gmail.com>
2019-03-16 21:44:51 +0200

    Added function

--- c/sum.c
+++ c/sum.c
@@ -1,12 +1,16 @@
 #include<stdio.h>
 
+int addition(int a, int b){
+	return a+b;
+}
+
 int main() {
    int x, y, result;
  
    printf("\nEnter the two numbers: ");
    scanf("%d %d", &x, &y);
  
-   result = x + y;
+   result = addition(x,y);
  
    printf("Addition of two number is: %d\n", result);
    return (0);
