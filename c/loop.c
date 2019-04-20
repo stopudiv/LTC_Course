1083cdf1621b66b45d606d0f0ffe4f5b7b693f71
Olga Grytsenko <ogrytsenko@gmail.com>
2019-04-20 12:45:30 +0300

    task about loops in C

--- c/loop.c
+++ c/loop.c
@@ -2,12 +2,24 @@
 int main () {
 	int a [] = {1,1,2,3,5,8,13,21};
 	int n = sizeof(a) / sizeof(a[0]);
+	int product =1;
 	printf("%d", n);
 	int step, sum = 0;
+	int sum1 = 0;
 	for (step = 0; step < n; ++step){
-	sum += a[step];
+	sum1 += a[step];
 	}
-	printf("\n%d\n", sum);
+	for (step =0; step <  n; step +=2){
+	product = product * a[step];
+	}
+	step = 0;
+	while (step < n){
+	sum += a[step];
+	++step;
+	}
+	printf("\n%d\n", sum1);
+	printf("%d\n", sum);
+	printf("%d\n", product);
 	return 0;
 	
 }
