f57d419cba0c2a6b2c5eb49810ba390f49777a90
Olga Grytsenko <ogrytsenko@gmail.com>
2019-05-03 12:04:20 +0300

    Arrays in JS

---
+++ js/array.js
@@ -0,0 +1,8 @@
+let cars = ["Saab", "Volvo", "BMW"];
+let houses =  new Array("One", "Two", "Three");
+console.log(cars[0]);
+console.log(houses[houses.length -1]);
+let arlen = cars.length;
+console.log(arlen);
+let numbers = [4, 6, 7, 8, 9];
+console.log(numbers);
