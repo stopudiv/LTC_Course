6c09c3e539bed05c6eadc78eaf6f9221aed21e4b
Olga Grytsenko <ogrytsenko@gmail.com>
2019-06-30 19:20:15 +0300

    Added extra parts

--- js/read_regexp.js
+++ js/read_regexp.js
@@ -5,3 +5,33 @@ let lineReader = require('readline').createInterface({
 lineReader.on('line', function (line){
 	console.log('Line from file: ', line);
 });
+
+let i;
+let count = 0;
+
+require('fs').createReadStream(process.argv[2])
+	.on('data', function(chunk) {
+		for (i=0; i < chunk.length; ++i)
+			if (chunk[i] == 10) count++;
+	})
+	.on('end', function() {
+	 console.log(count);
+	});
+
+
+const count1 = (str) => {
+  const re = /.../g
+  return ((str || '').match(re) || []).length
+}
+
+const count2 = (str) => {
+ const re = /^[a-z]+$/g
+ return ((str || '').match(re) || []).length
+}
+
+const str1 = 'abc, def, ghi, fgtht'
+
+
+console.log(`'${str1}' has ${count1(str1)} occurrences of pattern '/.../g'`)
+
+console.log(`'${str1}' has ${count1(str1)} occurrences of pattern '/...../g'`)
