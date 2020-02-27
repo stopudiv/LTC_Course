98076be5926639a01f31ab0c8cc7d64e3bed8527
Olga Grytsenko <ogrytsenko@gmail.com>
2020-02-28 01:41:22 +0200

    Corrected the fopen option for reading

--- c/byte_array.c
+++ c/byte_array.c
@@ -6,10 +6,14 @@ int main () {
 
 	FILE *fp;
 	char c [1024];
-	fp = fopen("um.um", "w+");
+	fp = fopen("um.um", "r");
 	fread(c, strlen(c) +1, 1, fp);
 	printf("%s\n", c);
 	fclose(fp);
+
+	int loop;
+   	for(loop = 0; loop < 1024; loop++)
+      	printf("%d ", c[loop]);
 
 	printf("Address of 'c' %pn",&c);
 	return(0);
